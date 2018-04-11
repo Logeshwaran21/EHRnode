c788a48fca8da26076406a7c3263215b6f0736e6
Arjunan95 <arjun.sankar@rapidqube.com>
2018-04-11 18:46:20 +0530

    changes done infdf

---
+++ route.js
@@ -0,0 +1,90 @@
+'use strict';
+
+
+
+const login = require('./functions/login');
+const registerUser = require('./functions/registerUser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+
+var Promises = require('promise');
+var path = require('path');
+
+module.exports = router => {
+
+    router.post('/registerUser', cors(), (req, res) => { 
+
+        const firstname = req.body.firstname;
+        console.log(firstname);
+        const lastname = req.body.lastname;
+        console.log(lastname);
+        const phonenumber = parseInt(req.body.phonenumber);
+        console.log(phonenumber);
+        const email = req.body.email;
+        console.log(email);
+        const password = req.body.password;
+        console.log(password);
+        const retypepassword = req.body.retypepassword;
+        console.log(retypepassword);
+        const usertype = req.body.usertype;
+        console.log(usertype);
+      
+
+        if (!firstname || !lastname || !phonenumber||  !email || !password || !retypepassword || !usertype) {
+
+            res
+                .status(400)
+                .json({
+                    message: 'Invalid Request !'
+                });
+
+        } else {
+
+            registerUser
+                .registerUser(firstname, lastname, phonenumber,email,password, retypepassword,usertype)
+                .then(result => {
+
+                    res.send({
+                        "message": "user has been registered successfully",
+                        "status": true,
+
+
+                    });
+
+
+                })
+                .catch(err => res.status(err.status).json({
+                    message: err.message
+                }).json({
+                    status: err.status
+                }));
+        }
+    });
+router.post('/login', cors(), (req, res) => {
+    console.log("entering login function in functions ");
+    const emailid = req.body.email;
+    console.log(emailid);
+    const passwordid = req.body.password;
+    console.log(passwordid);
+   
+   
+    login
+        .loginUser(emailid, passwordid)
+        .then(result => {   
+            console.log("result ===>>>",result.users.usertype)
+
+
+            res.send({
+                "message": "Login Successful",
+                "status": true,
+            });
+
+        })
+        .catch(err => res.status(err.status).json({
+            message: err.message
+        }).json({
+            status: err.status
+        }));
+
+});
+}
